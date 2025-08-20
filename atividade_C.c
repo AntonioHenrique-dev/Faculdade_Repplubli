7381312fb31e750f21b313a9e4a20804c65f126d
AntonioHenrique-dev <antoniohenriquesilvasantana@gmail.com>
2025-08-20 19:51:36 -0300

    Add files via upload

---
+++ atividade_C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+
+	int matricula;
+
+	printf("Qual sua matricula?\n");
+
+	scanf("%d",&matricula);
+
+	printf("Disciplina: Sistemas Computacionais\n");
+	printf("Turma: GPE17N20241\n");
+	printf("Nome: Antonio Henrique Silva Santana\n");
+	printf("Matricula: %d\n",matricula);
+	
+	return 0;
+
+;}
