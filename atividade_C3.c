07c6ef5eed8e62355637051e2abd30be3ee7422a
AntonioHenrique-dev <antoniohenriquesilvasantana@gmail.com>
2025-08-27 20:13:41 -0300

    Update atividade_C3.c

--- atividade_C3.c
+++ atividade_C3.c
@@ -29,12 +29,36 @@ scanf("%d",&cm);
 float kg= gramas/1000.0;
 float m= cm/100.0;
 
-imc= kg/m*m;
+imc= kg / (m*m);
 
-printf("imc=%f",&imc);
+printf( "%f \n", imc);
+if (imc < 18.5)
+{
+    printf("Abaixo do peso normal\n");
+}
 
-if imc < 18,5
-    printf("Abaixo do peso normal"
+else if(18.5 <= imc && imc <= 24.9)
+{
+    printf("Peso Normal\n");
+}
+else if (25 <= imc && imc <= 29.9)
+{
+    printf("Acima do peso\n");
+}
+else if (30 <= imc && imc <= 34.9)
+{
+    printf("Obesidade nivel 1\n");
+}
+else if (35 <= imc && imc <= 39.9)
+{
+    printf("Obesidade nivel 2\n");
+}
+else if (imc >= 40)
+{
+    printf("Obesidade nivel 3\n");
+}
+
+        
 // Outra forma: 
 //imc=(gramas/1000.0)/(cm/100.0)*(cm/100.0)
 //imc=(1.0 *gramas)/(cm*cm)*10.0
